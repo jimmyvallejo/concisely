fb79fe83386477392fde78a3c4b11b9ac197fdce
jimmyvallejo <jrv1295@gmail.com>
2025-01-13 17:49:47 -0500

    rename

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "./components/theme-provider";
 import { ApiKeysProvider } from "./context/key-provider";
-import Main from "./app/Main/page";
+import Main from "./app/main/page";
 import { Nav } from "./components/top-nav";
 
 function App() {
